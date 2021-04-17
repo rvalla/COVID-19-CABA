c5d621f826e649be06af541558c043d47150365f
Rodrigo Valla <rodrigovalla@protonmail.ch>
2021-04-17 01:00:55 -0300

    Adding proportions of cases by age...

--- d_analysis.py
+++ d_analysis.py
@@ -27,17 +27,31 @@ for i in range(2):
 
 columns = d_byage_cum[0].columns
 rows = d_byage_cum[0].index
+age_keys = ["<=10", "11-20", "21-30","31-40","41-50","51-60","61-70","71-80","81-90",">=91"]
 
 estimation_cum = pd.DataFrame(index=rows, columns=["confirmed", "deaths", "estimation", "knownratio"])
 estimation_avg = pd.DataFrame(index=rows, columns=["confirmed", "deaths", "estimation", "knownratio"])
 deathrate = pd.DataFrame(index = rows, columns=columns)
+c_age_ratios = pd.DataFrame(index = rows, columns=columns)
+d_age_ratios = pd.DataFrame(index = rows, columns=columns)
 estimation_cum.index.name = "FECHA"
 estimation_avg.index.name = "FECHA"
 deathrate.index.name = "FECHA"
+c_age_ratios.index.name = "FECHA"
+d_age_ratios.index.name = "FECHA"
 
 def build_death_rate(c_df, d_df, out_df):
 	for c in columns:
 		out_df[c] = d_df[c] / c_df[c]
+
+def build_age_ratios(in_df, out_df):
+	for k in age_keys:
+		out_df[k + "F"] = in_df[k + "F"] / in_df["TotalF"]
+		out_df[k + "M"] = in_df[k + "M"] / in_df["TotalM"]
+		out_df[k] = in_df[k] / in_df["Total"]
+	out_df["TotalF"] = in_df["TotalF"] / in_df["Total"]
+	out_df["TotalM"] = in_df["TotalM"] / in_df["Total"]
+	out_df["Total"] = in_df["Total"] / in_df["Total"]
 
 def build_estimation(c_df, d_df, out_df):
 	out_df["confirmed"] = c_df["Total"]
@@ -50,6 +64,8 @@ def build_estimation(c_df, d_df, out_df):
 def run():
 	print("-- Analysing cases data...", end="\n")
 	build_death_rate(d_byage_cum[0], d_byage_cum[1], deathrate)
+	build_age_ratios(d_byage_avg[0], c_age_ratios)
+	build_age_ratios(d_byage_avg[1], d_age_ratios)
 	build_estimation(d_byage_cum[0], d_byage_cum[1], estimation_cum)
 	build_estimation(d_byage_avg[0], d_byage_avg[1], estimation_avg)
 	save_processed_data()
@@ -58,6 +74,8 @@ def save_processed_data():
 	print("-- Preparing csv files...", end="\r")
 	name = "analysis_"
 	deathrate.to_csv(output_path + name + "deathrate.csv")
+	c_age_ratios.to_csv(output_path + name + "ratiosbyage_confirmed.csv")
+	d_age_ratios.to_csv(output_path + name + "ratiosbyage_deaths.csv")
 	estimation_cum.to_csv(output_path + name + "estimation_cum.csv")
 	estimation_avg.to_csv(output_path + name + "estimation_avg.csv")
 	print("-- Processed csv files saved!                 ", end="\n")
